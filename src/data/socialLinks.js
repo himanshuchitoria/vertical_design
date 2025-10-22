d9a6b7f82657a334574925c00edf9c2bc40081f9
Himanshu <119741346+himanshuchitoria@users.noreply.github.com>
2025-10-23 00:17:53 +0530

    Delete src directory

--- src/data/socialLinks.js
+++
@@ -1,7 +0,0 @@
-// src/data/socialLinks.js
-const socialLinks = {
-  instagram: "https://www.instagram.com/vertical_design_studio01?igsh=bWs4aWpxamRhbnBi",
-  facebook: "https://facebook.com/verticaldesignstudio",
-};
-
-export default socialLinks;
